c9ca93acc843e1f1bb44b93ea66ea72a6a41e18c
Abdo Omyan <omyanabdo677@gmail.com>
2025-07-19 16:50:29 +0400

    data layer

--- lib/featuers/auth/data/datasource/auth_datasource.dart
+++ lib/featuers/auth/data/datasource/auth_datasource.dart
@@ -1,1 +1,26 @@
+import 'package:car_appp/core/error/exception.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+abstract class AuthDatasource {
+  Future<User> login(String email, String password);
+}
+
+class AuthDatasourceImpl implements AuthDatasource {
+  @override
+  Future<User> login(String email, String password) async {
+    try {
+      final user = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if (user.user == null) {
+        throw ServerException('User Not Found');
+      } else {
+        return user.user!;
+      }
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(e.message ?? 'User Not Found');
+    }
+  }
+}
---
+++ lib/featuers/auth/data/repos/auth_repo_imp1.dart
@@ -0,0 +1,24 @@
+import 'package:car_appp/core/error/exception.dart';
+import 'package:car_appp/core/error/failure.dart';
+import 'package:car_appp/featuers/auth/data/datasource/auth_datasource.dart';
+import 'package:car_appp/featuers/auth/domain/repos/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  final AuthDatasource datasource;
+
+  AuthRepoImpl({required this.datasource});
+  @override
+  Future<Either<Failure, User>> login(String email, String password) async {
+    try {
+      return Right(await datasource.login(email, password));
+    } on ServerException catch (e) {
+      print(e);
+      return Left(ServerFailure(e.message));
+    } on Exception catch (e) {
+      print(e);
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
