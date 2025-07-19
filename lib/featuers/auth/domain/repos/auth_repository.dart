b9a6205d2b5ecab3a1843b4f2f5924b5990bd447
Abdo Omyan <omyanabdo677@gmail.com>
2025-07-19 16:45:37 +0400

    domain

--- lib/featuers/auth/domain/repos/auth_repository.dart
+++ lib/featuers/auth/domain/repos/auth_repository.dart
@@ -1,1 +1,7 @@
+import 'package:car_appp/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+abstract class AuthRepo {
+  Future<Either<Failure, User>> login(String email, String password);
+}
--- lib/featuers/auth/domain/usecase/login-user.dart
+++ lib/featuers/auth/domain/usecase/login-user.dart
@@ -1,1 +1,21 @@
+import 'package:car_appp/core/error/failure.dart';
+import 'package:car_appp/core/usecase/usecase.dart';
+import 'package:car_appp/featuers/auth/domain/repos/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+class LoginUsecase implements UseCase<User, LoginParams> {
+  final AuthRepo repo;
+
+  LoginUsecase({required this.repo});
+  @override
+  Future<Either<Failure, User>> call(LoginParams params) async {
+    return await repo.login(params.email, params.password);
+  }
+}
+
+class LoginParams {
+  final String email;
+  final String password;
+  LoginParams({required this.email, required this.password});
+}
